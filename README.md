264092e33e348a57f8aea754d04f057fdaeb347e
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2022-12-19 10:18:46 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # api-tools
 api-tools
+
+
+cd api-tools
+gunicorn --bind 0.0.0.0:5008 wsgi:app
+
+# Installation
+
+```
+git clone https:github.com/spelhate/api-tools
+cd api-tools
+python3 -m venv .venv
+pip install
+```
+
+## Configuration
+
+`cp .env.dist .env`
+
+Editer ensuite ce fichier
+
+## dev mode
+
+`flask run`
+
+## production mode
+
+`sudo cp tools-api.service /etc/systemd/system/tools-api.service`
+
+Then edit this file and set the correct USER and PORT
+
+```
+sudo mkdir /var/log/api-tools
+chown USER /var/log/api-tools
+sudo systemctl daemon-reload
+sudo systemctl enable api-tools
+sudo systemctl start api-tools
+```
---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+load_dotenv()  # loads variables from .env file into environment
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+url = os.environ.get("DATABASE_URL")  # gets variables from environment
+connection = psycopg2.connect(url)
+
+@app.get("/api/statistiques/statistiques")
+def getStatistiques():
+    STATS = ("select * from pastell.stats_historique limit 5;")
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(STATS)
+            results = cursor.fetchall();
+    return results, 200
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
