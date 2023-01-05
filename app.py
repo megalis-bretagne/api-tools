2fd3d9f4eaec0c3f99f1f86140e5741ca810e487
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2023-01-05 14:02:16 +0000

    Ajout flask-restx

--- app.py
+++ app.py
@@ -2,26 +2,37 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, Blueprint, url_for
 from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_restx import Resource, Api
 
 load_dotenv()  # loads variables from .env file into environment
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+api = Api(blueprint, version='0.1', title='Tools Api', description='API PRAE Tools', doc='/docs/')
+app.register_blueprint(blueprint)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+
+
 url = os.environ.get("DATABASE_URL")  # gets variables from environment
 connection = psycopg2.connect(url)
 
-@app.get("/api/statistiques/<string:table>")
-def getStatistiques(table):
-    STATS = (f"select * from pastell.{table};")
-    with connection:
-        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute(STATS)
-            results = cursor.fetchall();
-    return results, 200
+statistiques = api.namespace('statistiques', description='API Statistiques')
+
+
+@statistiques.route("/<string:table>")
+@api.doc(params={'table': 'Nom de la table'})
+class Statistiques(Resource):
+    def get(self, table):
+        STATS = (f"select * from pastell.{table};")
+        with connection:
+            with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+                cursor.execute(STATS)
+                results = cursor.fetchall();
+        return results, 200
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
