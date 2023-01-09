347dc41c438324a083909f6d19da8dc954a1802a
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2023-01-09 09:37:18 +0000

    Add url parameters

--- namespaces/statistiquesNS.py
+++ namespaces/statistiquesNS.py
@@ -1,25 +1,24 @@
 import json
 from flask import jsonify
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, reqparse
 from models.models import PublicationOpenData
 from utils import row2dict
 
 statistiques = Namespace('statistiques', description='API Statistiques')
 
+arguments_publication_open_data = reqparse.RequestParser()
+arguments_publication_open_data.add_argument('pageIndex', type=int, help='index de la page, commence par 1', default=1)
+arguments_publication_open_data.add_argument('pageSize', type=int, help='taille de la page', default=100)
+
 @statistiques.route("/publication_open_data")
-#@api.doc(params={'table': 'Nom de la table'})
 class Statistiques(Resource):
+    @statistiques.expect(arguments_publication_open_data)
     @statistiques.response(200, 'Success', statistiques.model('publication_opendata', PublicationOpenData().model))
     def get(self):
-        #STATS = ("select * from pastell.publication_open_data LIMIT 100;")
-        #with db.engine.connect() as con:
-        #    query = con.execute(STATS)
-        #return sql_to_json(query)
-        #result = PublicationOpenData.query.all()
-        #result = db.session.query(PublicationOpenData).all()
-        #result = PublicationOpenData.query.limit(100).all()
-        page = 1
-        size = 5
+        args = arguments_publication_open_data.parse_args()
+        print (args)
+        page = args['pageIndex']
+        size = args['pageSize']
         result = PublicationOpenData.query.paginate(page=page, per_page=size)
         data = {'response':'success','total': result.total, 'page': page, 'size': size, 'items': json.loads(json.dumps([row2dict(r) for r in result]))}
         return jsonify(**data)
