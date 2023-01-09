c52d24f2c117950cb7a4200fc3eec8f9c8d5634c
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2023-01-09 09:04:52 +0000

    Refacto

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_restx import Api
 
 from models.models import db
-from controllers.statistiquesCtrl import statistiques
+from namespaces.statistiquesNS import statistiques
 
-load_dotenv()  # loads variables from .env file into environment
+#loads variables from .env file into environment
+load_dotenv()
 
 app = Flask(__name__)
 blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -16,12 +17,13 @@ api = Api(blueprint, version='0.1', title='Tools Api', description='API PRAE Too
 app.register_blueprint(blueprint)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
-#Add API controllers
+#Add API namespaces
 api.add_namespace(statistiques)
 
 # configure the POSGRESQL Database
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-# initialize the app with the extension
+
+# initialize the app with the database
 db.init_app(app)
 
 if __name__ == "__main__":
