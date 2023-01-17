b4ce15731ad46e8a91453087e1fd569605f2abad
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2023-01-17 15:40:33 +0000

    Add optional arg siren for organigramme endpoint

--- namespaces/pastellNS.py
+++ namespaces/pastellNS.py
@@ -10,20 +10,21 @@ pastell = Namespace('pastell', description='API Pastell')
 arguments_organismes = reqparse.RequestParser()
 arguments_organismes.add_argument('pageIndex', type=int, help='index de la page, commence par 1', default=1)
 arguments_organismes.add_argument('pageSize', type=int, help='taille de la page', default=100)
-arguments_organismes.add_argument('siren', type=str, help='SIREN organisme')
 
-@pastell.route("/organismes")
+@pastell.route("/organismes/")
+@pastell.route("/organismes/<string:siren>")
 class Organismes(Resource):
     @pastell.expect(arguments_organismes)
     @pastell.response(200, 'Success', pastell.model('organisme', Organigramme().model))
     #@oidc.accept_token(require_token=True, scopes_required=['openid'])
-    def get(self):
+    def get(self, siren=None):
         args = arguments_organismes.parse_args()
         page = args['pageIndex']
         size = args['pageSize']
-        siren = args['siren']
-        print(siren)
-        result = Organigramme.query.paginate(page=page, per_page=size)
+        if siren:
+            result = Organigramme.query.filter(Organigramme.siren == siren).paginate(page=page, per_page=size)
+        else:
+            result = Organigramme.query.paginate(page=page, per_page=size)
         data = {'response':'success','total': result.total, 'page': page, 'size': size, 'items': json.loads(json.dumps([row2dict(r) for r in result]))}
         return jsonify(**data)
 
